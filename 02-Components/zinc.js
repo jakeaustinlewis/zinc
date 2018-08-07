dca95cef9aa6c8b58e2805c44eca2cd7110e5949
Jake Lewis <jakeaustinlewis@gmail.com>
2018-08-06 22:50:22 -0500

    added reviewStack functions

--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -8,22 +8,44 @@ const Zinc = {};
 
 (() => {
 
+    function reviewStackUserList() {
+        let elements = Array.from(document.getElementsByTagName('user-list'));
+        if(elements){
+            Zinc.registerComponent({
+                name: 'user-list',
+                templateFile: 'userlist',
+                data: user,
+                controller: hilight,
+            });
+            renderComponents(Zinc.components);
+        }
+    }
+    function reviewStackUserInfo() {
+        let elements = Array.from(document.getElementsByTagName('user-info'));
+        if(elements){
+            Zinc.registerComponent({
+                name: 'user-list',
+                templateFile: 'userlist',
+                data: user,
+                controller: hilight,
+            });
+            renderComponents(Zinc.components);
+        }
+    }
     function hilight() {
-            console.log(this);
                             
             this.firstElementChild.classList.toggle('hilight');
             // event.currentTarget.classList.toggle('hilight');
-
-            console.log('click');
     }
 
     // Zinc.registerComponent = function (elementName, templateFile, dataObject, controller) {
-        Zinc.registerComponent = function (configObj) {
+        Zinc.registerComponent = function(configObj) {
         if (!Zinc.components) {
             Zinc.components = {};
         }
 
         Zinc.components[configObj.name] = {
+
             name: configObj.name,
             templateFile: configObj.templateFile,
             data: configObj.data,
@@ -31,9 +53,11 @@ const Zinc = {};
         };
     }
 
+
     function renderComponent1(element, content, data, controller) {
         let elements = Array.from(document.getElementsByTagName(element));
-        console.log(element, content, data, controller);
+        console.log(elements);
+
 
         fetch(`${content}.html`)
             .then(content => content.text())
@@ -47,55 +71,57 @@ const Zinc = {};
                         return templateValueArr.reduce((acc, curr) => acc[curr], data)
                     })
 
+                    console.log(Array.from(element.children));
                     element.addEventListener('click', controller);
                     element.insertAdjacentHTML('beforeend', HTML);
+                    reviewStackUserList();
+                    reviewStackUserInfo()
                 })
             })
     }
 
     // passing components = Zinc.components
     function renderComponents(components) {
-        console.log(components);
         // looping through the keys and values of components object
         for (let component in components) {
 
             //passing content in object to function to render components (put content in html) 
             renderComponent1(
-
                 components[component].name,
                 components[component].templateFile,
                 components[component].data,
-                components[component].controller)
+                components[component].controller
+            )
         }
     }
 
     function init() {
-        // Zinc.registerComponent('user-item', 'user', Zinc.userData, controller);
-        // renderComponents(Zinc.components);
 
-        fetch('https://randomuser.me/api/?results=1')
+        fetch('https://randomuser.me/api/?results=3')
             .then(res => res.json())
             .then(data => {
                 data.results.forEach(user => {
 
                     Zinc.registerComponent({
-                        name: 'user-item',
+                        name: 'user-list',
+                        templateFile: 'userlist',
+                        data: user,
+                        controller: hilight,
+                    });
+                    renderComponents(Zinc.components);
+                })
+
+                data.results.forEach(user => {
+
+                    Zinc.registerComponent({
+                        name: 'user-info',
                         templateFile: 'user',
                         data: user,
                         controller: hilight,
-                        // ... and so on
                     });
                     renderComponents(Zinc.components);
                 })
             })
-
-
-
-        // renderTemplate2(userTemplate, data.results);
-        // renderTemplate('user', data.results);
-
-
-
     }
 
     document.addEventListener('DOMContentLoaded', init);
